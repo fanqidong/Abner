e1649855a78581c546a541a41fda902be9d93c1b
fanqidong <1425434413@qq.com>
2019-10-11 15:16:56 +0800

    update some styles

--- src/api/request.js
+++ src/api/request.js
@@ -1,237 +1,220 @@
-import axios from "axios"
-import gobalConfig from "../config/global.config"
-import Router from "../router"
-// import AV from 'leancloud-storage'
-const {
-  blog,
-  token,
-  creator,
-  isDev
-} = gobalConfig
-const access_token = `access_token=${token.join("")}`
+import axios from 'axios'
+import gobalConfig from '../config/global.config'
+import Router from '../router'
+import AV from 'leancloud-storage'
+const { blog, token, creator, isDev } = gobalConfig
+const access_token = `access_token=${token.join('')}`
 const open = `creator=${creator}&state=open&${access_token}`
 const closed = `creator=${creator}&state=closed&${access_token}`
-const photoUrl = "https://api.github.com/repos/fanqidong/Blog-Back-Up/contents/min_photos"
+const photoUrl = 'https://api.github.com/repos/fanqidong/Blog-Back-Up/contents/min_photos'
 // 状态码检测
 const checkStatus = res => {
-  if (res.status >= 200 && res.status < 300) return res
-  const error = new Error(res.statusText)
-  error.res = res
-  throw error
+    if (res.status >= 200 && res.status < 300) return res
+    const error = new Error(res.statusText)
+    error.res = res
+    throw error
 }
 
 const httpGet = (url, params) => {
-  return new Promise((resolve, reject) => {
-    axios({
-        method: "get",
-        baseURL: blog,
-        url,
-        params,
-        timeout: 20000,
-        headers: {
-          "X-Requested-With": "XMLHttpRequest"
-        }
-      })
-      .then(res => {
-        resolve(res.data)
-      })
-      .catch(err => {
-        reject(err)
-      })
-  })
+    return new Promise((resolve, reject) => {
+        axios({
+            method: 'get',
+            baseURL: blog,
+            url,
+            params,
+            timeout: 20000,
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+        })
+            .then(res => {
+                resolve(res.data)
+            })
+            .catch(err => {
+                reject(err)
+            })
+    })
 }
-
 
 export const queryPhoto = async () => {
-  try {
-    const res = await httpGet(photoUrl)
-    return res
-  } catch (error) {
-    console.log(error)
-  }
+    try {
+        const res = await httpGet(photoUrl)
+        return res
+    } catch (error) {
+        console.log(error)
+    }
 }
 // 获取文章列表
-export const queryPosts = async ({
-  page = 1,
-  pageSize = "",
-  filter = ""
-}) => {
-  try {
-    const url = `${blog}/issues?${open}&page=${page}&per_page=${pageSize}${filter}`
-    const res = await fetch(url)
-    checkStatus(res)
-    const data = await res.json()
-    return data
-  } catch (error) {
-    console.log(error)
-  }
+export const queryPosts = async ({ page = 1, pageSize = '', filter = '' }) => {
+    try {
+        const url = `${blog}/issues?${open}&page=${page}&per_page=${pageSize}${filter}`
+        const res = await fetch(url)
+        checkStatus(res)
+        const data = await res.json()
+        return data
+    } catch (error) {
+        console.log(error)
+    }
 }
 // 获取文章详情
 export const queryPost = async number => {
-  try {
-    const url = `${blog}/issues/${number}?${open}`
-    const res = await fetch(url)
-    checkStatus(res)
-    const data = await res.json()
-    return data
-  } catch (error) {
-    console.log(error)
-  }
+    try {
+        const url = `${blog}/issues/${number}?${open}`
+        const res = await fetch(url)
+        checkStatus(res)
+        const data = await res.json()
+        return data
+    } catch (error) {
+        console.log(error)
+    }
 }
 // 获取分类
 export const queryCategory = async () => {
-  try {
-    const url = `${blog}/milestones?${access_token}`
-    const res = await fetch(url)
-    checkStatus(res)
-    const data = await res.json()
-    return data
-  } catch (error) {
-    console.log(error)
-  }
+    try {
+        const url = `${blog}/milestones?${access_token}`
+        const res = await fetch(url)
+        checkStatus(res)
+        const data = await res.json()
+        return data
+    } catch (error) {
+        console.log(error)
+    }
 }
 // 获取标签
 export const queryLabel = async () => {
-  try {
-    const url = `${blog}/labels?${access_token}`
-    const response = await fetch(url)
-    checkStatus(response)
-    const data = await response.json()
-    return data
-  } catch (err) {
-    console.log(err)
-  }
+    try {
+        const url = `${blog}/labels?${access_token}`
+        const response = await fetch(url)
+        checkStatus(response)
+        const data = await response.json()
+        return data
+    } catch (err) {
+        console.log(err)
+    }
 }
 // 获取心情
-export const queryMood = async ({
-  page = 1,
-  pageSize = ''
-}) => {
-  try {
-    const url = `${blog}/issues?${closed}&labels=mood&&page=${page}&per_page=${pageSize}`
-    const res = await fetch(url)
-    checkStatus(res)
-    const data = await res.json()
-    return data
-  } catch (error) {
-    console.log(error)
-  }
+export const queryMood = async ({ page = 1, pageSize = '' }) => {
+    try {
+        const url = `${blog}/issues?${closed}&labels=mood&&page=${page}&per_page=${pageSize}`
+        const res = await fetch(url)
+        checkStatus(res)
+        const data = await res.json()
+        return data
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 // 获取友链 && 关于
 export const queryType = async type => {
-  try {
-    const url = `${blog}/issues?${closed}&labels=${type}`
-    const response = await fetch(url)
-    checkStatus(response)
-    const data = await response.json()
-    return data[0]
-  } catch (error) {
-    console.log(error)
-  }
+    try {
+        const url = `${blog}/issues?${closed}&labels=${type}`
+        const response = await fetch(url)
+        checkStatus(response)
+        const data = await response.json()
+        return data[0]
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 //  获取文章热度
 export const queryHot = async (postList, add) => {
-  return new Promise(resolve => {
-    // if (isDev || location.host.includes("192")) return resolve(postList)
-    const seq = postList.map(item => {
-      return new Promise(resolve => {
-        const query = new AV.Query("Counter")
-        const Counter = AV.Object.extend("Counter")
-        const {
-          title,
-          id
-        } = item
-        query.equalTo("id", id)
-        query
-          .find()
-          .then(res => {
-            if (res.length > 0) {
-              //已存在热度直接返回
-              const counter = res[0]
-              // 是否增接热度
-              if (add) {
-                counter
-                  .increment("time", 1)
-                  .save(null, {
-                    fetchWhenSave: true
-                  })
-                  .then(counter => {
-                    item.times = counter.get("time")
-                    resolve(item)
-                  })
-                  .catch(console.error)
-              } else {
-                item.times = counter.get("time")
-                resolve(item)
-              }
-            } else {
-              // 不存在则新建
-              const newCounter = new Counter()
-              newCounter.set("title", title)
-              newCounter.set("id", id)
-              newCounter.set("time", 1)
-              newCounter.set("site", location.href)
-              newCounter
-                .save()
-                .then(() => resolve(item))
-                .catch(error => {
-                  console.log(error)
-                })
-            }
-          })
-          .catch(error => {
-            console.log(error)
-          })
-      }).catch(error => {
+    return new Promise(resolve => {
+        // if (isDev || location.host.includes("192")) return resolve(postList)
+        const seq = postList.map(item => {
+            return new Promise(resolve => {
+                const query = new AV.Query('Counter')
+                const Counter = AV.Object.extend('Counter')
+                const { title, id } = item
+                query.equalTo('id', id)
+                query
+                    .find()
+                    .then(res => {
+                        if (res.length > 0) {
+                            //已存在热度直接返回
+                            const counter = res[0]
+                            // 是否增接热度
+                            if (add) {
+                                counter
+                                    .increment('time', 1)
+                                    .save(null, {
+                                        fetchWhenSave: true
+                                    })
+                                    .then(counter => {
+                                        item.times = counter.get('time')
+                                        resolve(item)
+                                    })
+                                    .catch(console.error)
+                            } else {
+                                item.times = counter.get('time')
+                                resolve(item)
+                            }
+                        } else {
+                            // 不存在则新建
+                            const newCounter = new Counter()
+                            newCounter.set('title', title)
+                            newCounter.set('id', id)
+                            newCounter.set('time', 1)
+                            newCounter.set('site', location.href)
+                            newCounter
+                                .save()
+                                .then(() => resolve(item))
+                                .catch(error => {
+                                    console.log(error)
+                                })
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+            }).catch(error => {
+                console.log(error)
+            })
+        })
+        Promise.all(seq)
+            .then(data => resolve(data))
+            .catch(error => {
+                console.log(error)
+            })
+    }).catch(error => {
         console.log(error)
-      })
     })
-    Promise.all(seq)
-      .then(data => resolve(data))
-      .catch(error => {
-        console.log(error)
-      })
-  }).catch(error => {
-    console.log(error)
-  })
 }
 
 //  为网站点赞
 export const queryLikeSite = async type => {
-  if (isDev || location.host.includes("192")) return
-  return new Promise(resolve => {
-    const query = new AV.Query("Counter")
-    const Counter = AV.Object.extend("Counter")
-    query.equalTo("title", "site")
-    query
-      .first()
-      .then(res => {
-        if (res) {
-          if (type === "getTimes") {
-            resolve(res.get("time"))
-          } else {
-            res
-              .increment("time", 1)
-              .save(null, {
-                fetchWhenSave: true
-              })
-              .then(counter => resolve(counter.get("time")))
-              .catch(console.error)
-          }
-        } else {
-          // 不存在则新建
-          const newcounter = new Counter()
-          newcounter.set("title", "site")
-          newcounter.set("time", 1)
-          newcounter.set("site", location.href)
-          newcounter
-            .save()
-            .then(counter => resolve(counter.get("time")))
+    if (isDev || location.host.includes('192')) return
+    return new Promise(resolve => {
+        const query = new AV.Query('Counter')
+        const Counter = AV.Object.extend('Counter')
+        query.equalTo('title', 'site')
+        query
+            .first()
+            .then(res => {
+                if (res) {
+                    if (type === 'getTimes') {
+                        resolve(res.get('time'))
+                    } else {
+                        res.increment('time', 1)
+                            .save(null, {
+                                fetchWhenSave: true
+                            })
+                            .then(counter => resolve(counter.get('time')))
+                            .catch(console.error)
+                    }
+                } else {
+                    // 不存在则新建
+                    const newcounter = new Counter()
+                    newcounter.set('title', 'site')
+                    newcounter.set('time', 1)
+                    newcounter.set('site', location.href)
+                    newcounter
+                        .save()
+                        .then(counter => resolve(counter.get('time')))
+                        .catch(console.error)
+                }
+            })
             .catch(console.error)
-        }
-      })
-      .catch(console.error)
-  }).catch(console.error)
+    }).catch(console.error)
 }
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,172 +1,171 @@
 <template>
-  <div class="home">
-    <Bg :opacity="scrollRate" :bg-url="bgUrl"/>
-    <section class="home-wrapper row">
-      <div class="site-recommend bfff tl">
-        <p>
-          <i class="iconfont icon-recommend"></i>你好啊，欢迎访问我的博客！😘
-        </p>
-        <p>今天是{{today.time}}——{{today.week}}</p>
-        <p>元气满满的一天哦！</p>
-      </div>
-      <section class="article">
-        <div class="article-list" v-if="posts.length">
-          <article
-            data-aos="fade-up"
-            data-aos-once="true"
-            v-for="(post,index) in posts"
-            :key="post.id"
-            class="article-wrapper clearfix"
-            :class="{'article-float':index%2!=0}"
-            @click="goDetail(post.number)"
-          >
-            <div class="article-cover">
-              <a href="javascript:;" class="article-link">
-                <img v-lazy="post.cover.src" :alt="post.cover.text">
-              </a>
-              <!-- 发表时间 -->
-              <div class="article-date flex-center flex-column absolute-full">
-                <span class="month font18">{{post.timeinfo.month}}</span>
-                <span class="time">
-                  <i class="iconfont icon-calendar"></i>
-                  {{post.timeinfo.date}}
-                </span>
-              </div>
+    <div class="home">
+        <Bg :opacity="scrollRate" :bg-url="bgUrl" />
+        <section class="home-wrapper row">
+            <div class="site-recommend bfff tl">
+                <p>
+                    <i class="iconfont icon-recommend"></i>你好啊，欢迎访问我的博客！😘
+                </p>
+                <p>今天是{{today.time}}——{{today.week}}</p>
+                <p>元气满满的一天哦！</p>
             </div>
-            <!-- 文章内容  Start -->
-            <div class="article-content flex-around flex-column">
-              <!-- 文章标题 -->
-              <div class="article-title font20 c000">{{post.title}}</div>
-              <!-- 简介 -->
-              <span class="article-desc overflow3 c8a8">{{post.desc}}</span>
-              <div class="article-info flex-between c666">
-                <!-- 热度 -->
-                <span>
-                  <i class="iconfont icon-hot"></i>
-                  <em>{{post.times?post.times:1}}°C</em>
-                </span>
-                <!-- 归档 -->
-                <span>
-                  <i class="iconfont icon-guidangxiangmu"></i>
-                  <em>{{post.milestone.title }}</em>
-                </span>
-                <!-- 标签 -->
-                <span class="archive">
-                  <i class="iconfont icon-biaoqian"></i>
-                  <em v-for="label in post.labels.slice(0,2)" :key="label.id">{{label.name}}</em>
-                </span>
-              </div>
-            </div>
-            <!-- 文章内容 End -->
-          </article>
-          <div class="line">我是有底线的</div>
-        </div>
-        <partLoading v-else/>
-      </section>
-    </section>
-  </div>
+            <section class="article">
+                <div class="article-list" v-if="posts.length">
+                    <article
+                        data-aos="fade-up"
+                        data-aos-once="true"
+                        v-for="(post,index) in posts"
+                        :key="post.id"
+                        class="article-wrapper clearfix"
+                        :class="{'article-float':index%2!=0}"
+                        @click="goDetail(post.number)"
+                    >
+                        <div class="article-cover">
+                            <a href="javascript:;" class="article-link">
+                                <img v-lazy="post.cover.src" :alt="post.cover.text" />
+                            </a>
+                            <!-- 发表时间 -->
+                            <div class="article-date flex-center flex-column absolute-full">
+                                <span class="month font18">{{post.timeinfo.month}}</span>
+                                <span class="time">
+                                    <i class="iconfont icon-calendar"></i>
+                                    {{post.timeinfo.date}}
+                                </span>
+                            </div>
+                        </div>
+                        <!-- 文章内容  Start -->
+                        <div class="article-content flex-around flex-column">
+                            <!-- 文章标题 -->
+                            <div class="article-title font20 c000">{{post.title}}</div>
+                            <!-- 简介 -->
+                            <span class="article-desc overflow3 c8a8">{{post.desc}}</span>
+                            <div class="article-info flex-between c666">
+                                <!-- 热度 -->
+                                <span>
+                                    <i class="iconfont icon-hot"></i>
+                                    <em>{{post.times?post.times:1}}°C</em>
+                                </span>
+                                <!-- 归档 -->
+                                <span>
+                                    <i class="iconfont icon-guidangxiangmu"></i>
+                                    <em>{{post.milestone.title }}</em>
+                                </span>
+                                <!-- 标签 -->
+                                <span class="archive">
+                                    <i class="iconfont icon-biaoqian"></i>
+                                    <em
+                                        v-for="label in post.labels.slice(0,2)"
+                                        :key="label.id"
+                                    >{{label.name}}</em>
+                                </span>
+                            </div>
+                        </div>
+                        <!-- 文章内容 End -->
+                    </article>
+                    <div class="line">我是有底线的</div>
+                </div>
+                <partLoading v-else />
+            </section>
+        </section>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import store from "@/store"
-import { mapState } from "vuex"
-import Aos from "aos"
-import { getScrollTop, addEvent } from "@/utils/dom"
-import MarkDown from "@/components/Markdown"
-import partLoading from "@/components/partLoading"
-import Bg from "@/components/Background"
-import dayjs from "dayjs"
+import store from '@/store'
+import { mapState } from 'vuex'
+import Aos from 'aos'
+import { getScrollTop, addEvent } from '@/utils/dom'
+import MarkDown from '@/components/Markdown'
+import partLoading from '@/components/partLoading'
+import Bg from '@/components/Background'
+import dayjs from 'dayjs'
 
 export default {
-  name: "Home",
-  components: {
-    MarkDown,
-    Bg,
-    partLoading
-  },
-  data() {
-    return {
-      scrollRate: "",
-      bgUrl: "https://zankyo.cc/wp-content/themes/Sakura/cover/gallery/66041517_p0.png",
-      toWeek: {
-        Monday: "星期一",
-        Tuesday: "星期二",
-        Wednesday: "星期三",
-        Thursday: "星期四",
-        Friday: "星期五",
-        Saturday: "星期六",
-        Sunday: "星期日"
-      }
-    }
-  },
-  computed: {
-    ...mapState({
-      posts: state => state.posts,
-      hasMore: state => state.hasMore,
-      homeScrollTop: state => state.homeScrollTop
-    }),
-    today() {
-      let time = dayjs(new Date()).format("YYYY年MM月DD日")
-      let week = this.toWeek[dayjs(new Date()).format("dddd")]
-      return {
-        time,
-        week
-      }
-    }
-  },
-  methods: {
-    //  获取文章列表
-    async getPosts() {
-      await store.dispatch("queryPosts", { type: "article" })
+    name: 'Home',
+    components: {
+        MarkDown,
+        Bg,
+        partLoading
     },
-    // 前往文章详情页
-    goDetail(number) {
-      this.$router.push({ name: "ArticleDetail", params: { number } })
+    data() {
+        return {
+            scrollRate: '',
+            bgUrl: 'https://zankyo.cc/wp-content/themes/Sakura/cover/gallery/66041517_p0.png',
+            toWeek: {
+                Monday: '星期一',
+                Tuesday: '星期二',
+                Wednesday: '星期三',
+                Thursday: '星期四',
+                Friday: '星期五',
+                Saturday: '星期六',
+                Sunday: '星期日'
+            }
+        }
     },
-    // 滚动比率
-    handleRate() {
-      addEvent("scroll", () => {
-        let rate = (getScrollTop() * 1.5) / window.innerHeight
-        this.scrollRate = 1 - rate < 0 ? 0 : 1 - rate
-      })
+    computed: {
+        ...mapState(['posts', 'hasMore', 'homeScrollTop']),
+        today() {
+            let time = dayjs(new Date()).format('YYYY年MM月DD日')
+            let week = this.toWeek[dayjs(new Date()).format('dddd')]
+            return {
+                time,
+                week
+            }
+        }
+    },
+    methods: {
+        //  获取文章列表
+        async getPosts() {
+            await store.dispatch('queryPosts', { type: 'article' })
+        },
+        // 前往文章详情页
+        goDetail(number) {
+            this.$router.push({ name: 'ArticleDetail', params: { number } })
+        },
+        // 滚动比率
+        handleRate() {
+            addEvent('scroll', () => {
+                let rate = (getScrollTop() * 1.5) / window.innerHeight
+                this.scrollRate = 1 - rate < 0 ? 0 : 1 - rate
+            })
+        }
+    },
+    // 路由进入判断是详情页返回首页还是直接返回首页
+    beforeRouteEnter(to, from, next) {
+        next(vm => {
+            if (from.name == 'ArticleDetail' && vm.$route.params.type == 'ArticleDetail') {
+                window.scrollTo(0, vm.homeScrollTop)
+            } else {
+                window.scrollTo(0, 0)
+            }
+        })
+    },
+    //在页面离开时记录滚动位置
+    beforeRouteLeave(to, from, next) {
+        this.scrollTop = getScrollTop()
+        store.commit('recordScroll', { homeScrollTop: this.scrollTop })
+        next()
+    },
+    created() {
+        if (!this.posts.length) {
+            this.getPosts()
+        }
+        Aos.init({
+            duration: 1000,
+            easing: 'ease-out',
+            debounceDelay: 200,
+            offset: 20
+        }),
+            setTimeout(Aos.refresh, 600)
+    },
+    mounted() {
+        this.handleRate()
     }
-  },
-  // 路由进入判断是详情页返回首页还是直接返回首页
-  beforeRouteEnter(to, from, next) {
-    next(vm => {
-      if (from.name == "ArticleDetail" && vm.$route.params.type == "ArticleDetail") {
-        window.scrollTo(0, vm.homeScrollTop)
-      } else {
-        window.scrollTo(0, 0)
-      }
-    })
-  },
-  //在页面离开时记录滚动位置
-  beforeRouteLeave(to, from, next) {
-    this.scrollTop = getScrollTop()
-    store.commit("recordScroll", { homeScrollTop: this.scrollTop })
-    next()
-  },
-  created() {
-    if (!this.posts.length) {
-      this.getPosts()
-    }
-    Aos.init({
-      duration: 1000,
-      easing: "ease-out",
-      debounceDelay: 200,
-      offset: 20
-    }),
-      setTimeout(Aos.refresh, 600)
-  },
-  mounted() {
-    this.handleRate()
-  }
 }
 </script>
 <style lang="scss" scoped>
-@import "./index.scss";
+@import './index.scss';
 </style>
 
 
